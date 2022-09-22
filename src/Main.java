912243a34687f53ac6d72081008fccd19500cae6
Colton Fox <coltonfox@coltons-mbp.wifi.uc.edu>
2022-09-22 14:56:42 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int age = 20;
+
+        if(age >= 21)
+        {
+            System.out.println("You are over 21 here's your wristband ");
+        }
+        else
+        {
+        }
+    }
+}
